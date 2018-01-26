3e2387e4f4645a7012358a0a144354d8bb09b8ac
knasa21 <jellyfish.mizukura@gmail.com>
2018-01-27 02:06:50 +0900

    Update Project folder D and D in this file.bat
    finished!の!を消した

--- Project folder D and D in this file.bat
+++ Project folder D and D in this file.bat
@@ -2,7 +2,7 @@
 echo %~dp0
 echo make dir......
 powershell -NoProfile -ExecutionPolicy Unrestricted %~dp0UnityDirectorySetting.ps1 "'%~dp0' '%1' '%~n1'"
-echo finishedI
+echo finished
 
 #pause > nul
 exit
